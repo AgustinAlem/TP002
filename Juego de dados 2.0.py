b9bbe58a7140936db2b5a3a7bcc2c2e7ca3eb64d
Agustin Alem <82903483+AgustinAlem@users.noreply.github.com>
2021-06-09 18:03:21 -0300

    Update Juego de dados 2.0.py

--- Juego de dados 2.0.py
+++ Juego de dados 2.0.py
@@ -12,4 +12,55 @@
 #si el ganador es quién tuvo mayor porcentaje de aciertos
 #Si algún jugador ganó en 3 turnos seguidos
 
-jugador
+import random
+jugador1 = input("Ingrese el nombre del primer jugador: ")
+jugador2 = input("Ingrese el nombre del segundo jugador: ")
+
+puntajeAlcanzar = int(input("Ingrese el puntaje a ser alcanzado: "))
+
+puntajej1 = 0
+puntajej2 = 0
+
+while puntajeAlcanzar != puntajej1 or puntajeAlcanzar != puntajej2:
+
+#JUGADOR 1
+    opcion = int(input("Ingrese el número de la opción elegida: "))
+    print("Opciones:")
+    print("         1: par.")
+    print("         2: impar.")
+
+    if opcion != 1 or opcion != 2:
+       print("Opción no válida")
+
+    else:
+        dado1j1 = random.randint(1,6)
+        dado2j1 = random.randint(1,6)
+        dado3j1 = random.randint(1,6)
+
+        if ((dado1j1 + dado2j1 + dado3j1) % 2) == 0 and opcion == 1:
+            puntajej1 += max(dado1j1, dado2j1, dado3j1) * 2
+
+        elif((dado1j1 + dado2j1 + dado3j1) % 2) == 0 and opcion == 2:
+            puntajej1 = puntajej1 - min(dado1j1, dado2j1, dado3j1)
+
+        elif((dado1j1 + dado2j1 + dado3j1) % 2) == 1 and opcion == 2:
+            puntajej1 += max(dado1j1, dado2j1, dado3j1) * 2
+
+        elif((dado1j1 + dado2j1 + dado3j1) % 2) == 1 and opcion == 1:
+            puntajej1 = puntajej1 - min(dado1j1, dado2j1, dado3j1)
+
+
+#JUGADOR 2
+
+    opcionj2 = int(input("Ingrese el número de la opción elegida: "))
+    print("Opciones:")
+    print("         1: par.")
+    print("         2: impar.")
+
+    if opcionj2 != 1 or opcionj2 != 2:
+        print("Opción no válida")
+
+    else:
+        dado1j2 = random.randint(1, 6)
+        dado2j2 = random.randint(1, 6)
+        dado3j2 = random.randint(1, 6)
