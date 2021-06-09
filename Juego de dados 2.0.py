d3b346c82355777fb6c7a89af87e5b5558ed38d7
Agustin Alem <82903483+AgustinAlem@users.noreply.github.com>
2021-06-09 17:54:49 -0300

    Add files via upload

---
+++ Juego de dados 2.0.py
@@ -0,0 +1,15 @@
+# apostar par impar
+# paridad = max*2
+# no paridad = - min
+#puntaje a ser obtenido
+#se juega hasta obtener el puntaje elegido
+#mostrar nombre y puntaje de cada jugador
+#si tienen mismo puntaje gana el jugador con más turnos ganados
+#q jugadas realizadas
+#si hubo empate en el puntaje en algún turno
+#puntaje promedio obtenido por cada jugador
+#porcentaje de aciertos para c jugador
+#si el ganador es quién tuvo mayor porcentaje de aciertos
+#Si algún jugador ganó en 3 turnos seguidos
+
+jugador
